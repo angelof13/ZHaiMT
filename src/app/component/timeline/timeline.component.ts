3cb838e0a44ddb3c2486e2c7a32b5908110678c9
ZhuHui <1076734229@qq.com>
2020-06-22 22:26:15 +0800

    动态获取宽高

--- src/app/component/timeline/timeline.component.ts
+++ src/app/component/timeline/timeline.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./timeline.component.scss']
 })
 export class TimelineComponent implements OnInit {
-
-  constructor() { }
+  
+  value_window={H:document.body.clientHeight,W:document.body.clientWidth};
+  constructor() {}
 
   ngOnInit(): void {
+    let that=this;
+    window.onresize = function () {
+      //这里表示当窗口大小发生变化时所做的事，也就是说可以对多个图表进行大小调整
+      that.value_window.H=document.body.clientHeight;
+      that.value_window.W=document.body.clientWidth;
+    }
   }
 
 }
