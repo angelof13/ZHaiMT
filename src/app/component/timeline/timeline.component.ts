da1ffddcb23e6154aaebfe98d9f416cab8ca216d
ZhuHui <1076734229@qq.com>
2020-06-27 23:56:50 +0800

    决定使用canvas，并抽离绘制方法

--- src/app/component/timeline/timeline.component.ts
+++ src/app/component/timeline/timeline.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import {DrawService } from '../../service/draw.service';
+import {line_info} from '../../service/draw.service';
 @Component({
   selector: 'app-timeline',
   templateUrl: './timeline.component.html',
@@ -7,16 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimelineComponent implements OnInit {
   
-  value_window={H:document.body.clientHeight,W:document.body.clientWidth};
-  constructor() {}
+  canvas:HTMLCanvasElement;
+  value_line:line_info;
+
+  constructor(private draw:DrawService) { }
 
   ngOnInit(): void {
-    let that=this;
-    window.onresize = function () {
-      //这里表示当窗口大小发生变化时所做的事，也就是说可以对多个图表进行大小调整
-      that.value_window.H=document.body.clientHeight;
-      that.value_window.W=document.body.clientWidth;
-    }
+    this.value_line={line_num:1,length:1,HorV:1};
+    //this.draw.draw_line(<HTMLCanvasElement>this.canvas,this.value_line);
   }
 
+  ngAfterViewInit(): void {
+    let that=this;
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    this.canvas = <HTMLCanvasElement>document.getElementById("myCanvas");
+    that.draw.draw_line(that.canvas,this.value_line);
+    
+    window.onresize = function () {
+      //这里表示当窗口大小发生变化时所做的事，也就是说可以对多个图表进行大小调整
+      that.draw.draw_line(that.canvas,this.value_line);
+    }
+  }
 }
