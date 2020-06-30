e6e08ddf5d11b9d7a997a8c158fc3443a51b6c27
ZhuHui <1076734229@qq.com>
2020-07-01 03:02:15 +0800

    完成画图,需要将resize中的upDate和drawEquidistantLine方法调用改为同步,否则会导致canvas宽高更新异常

--- src/app/component/timeline/timeline.component.ts
+++ src/app/component/timeline/timeline.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { fromEvent } from 'rxjs';
+import { fromEvent, Observable } from 'rxjs';
 
 import * as datatype from './timeline_datatype';
 
-import {Canvas_info, line_info} from '../../service/draw.service';
-import {DrawService } from '../../service/draw.service';
+import { canvas_info, horizontal_vertical as HV, line_info } from '../../service/draw.service';
+import { DrawService } from '../../service/draw.service';
 
 
 @Component({
@@ -14,34 +14,34 @@ import {DrawService } from '../../service/draw.service';
 })
 export class TimelineComponent implements OnInit {
 
-  value_line:line_info;
-  canvas:Canvas_info={canvas_height:200 ,canvas_width:300};
-  
-  constructor(private draw:DrawService) { }
+  value_line: line_info;
+  canvas: canvas_info = { canvas_height: 0, canvas_width: 0 };
 
-  private update():void{
-    if(this.canvas.canvas_event){
-      this.canvas.canvas_height=this.canvas.canvas_event.parentElement.offsetHeight;
-      this.canvas.canvas_width=this.canvas.canvas_event.parentElement.offsetWidth;
+  constructor(private draw: DrawService) { }
+
+  private upDate(): void {
+    if (this.canvas.canvas_event) {
+      this.canvas.canvas_height = this.canvas.canvas_event.parentElement.offsetHeight;
+      this.canvas.canvas_width = this.canvas.canvas_event.parentElement.offsetWidth;
     }
   }
 
   ngOnInit(): void {
-    this.update();
-    this.value_line={line_num:4,length:100,HorV:1};
+    this.upDate();
+    this.value_line = { line_num: 12, length: 0.8, HorV: HV.v };
   }
-
+  let
   ngAfterViewInit(): void {
-    let that=this;
+    let that = this;
     this.canvas.canvas_event = <HTMLCanvasElement>document.getElementById("myCanvas");
-    this.update();
+    this.upDate();
 
-    that.draw.draw_line(this.canvas,this.value_line);
-    
-    fromEvent(window,'resize').subscribe((event)=>{
+    that.draw.drawEquidistantLine(this.canvas, this.value_line);
+
+    fromEvent(window, 'resize').subscribe((event) => {
       //这里表示当窗口大小发生变化时所做的事，也就是说可以对多个图表进行大小调整
-      that.update();
-      that.draw.draw_line(that.canvas,this.value_line);
+      that.upDate();
+      that.draw.drawEquidistantLine(that.canvas, this.value_line);
     })
   }
 }
--- src/app/service/draw.service.ts
+++ src/app/service/draw.service.ts
@@ -1,44 +1,68 @@
 import { Injectable } from '@angular/core';
 
-export interface Canvas_info {
-  canvas_event?:HTMLCanvasElement;
-  canvas_height:number;
-  canvas_width:number;
+export interface canvas_info {
+  canvas_event?: HTMLCanvasElement;
+  canvas_height: number;
+  canvas_width: number;
 }
 
-export interface line_info {
-  line_num:number;
-  length:number;
-  HorV:number;
+export enum horizontal_vertical {
+  h, v
 }
+export interface line_info {
+  line_num: number;
+  length: number;
+  HorV: horizontal_vertical;
+}
+
+const init_point = { x: 40, y: 40 };
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class DrawService {
-  
+
   constructor() { }
 
   /**
-  * @param {Canvas_info} canvas canvas信息
+  * @param {canvas_info} canvas canvas信息
   */
-  draw_line(canvas:Canvas_info,Info_line:line_info):Boolean{
-    if(!canvas.canvas_event){
-      console.log("none");
+  drawEquidistantLine(canvas: canvas_info, line: line_info): Boolean {
+    if (!canvas.canvas_event) {
       return false;
     }
+
     canvas.canvas_event.height = canvas.canvas_height;
     canvas.canvas_event.width = canvas.canvas_width;
-    let draw_map=canvas.canvas_event.getContext("2d");
-    draw_map.lineCap="round";
-    draw_map.beginPath();
-    draw_map.moveTo(20,20);
-    draw_map.lineTo(20,50);
-    draw_map.lineTo(90,50);
-    draw_map.lineTo(90,500);
-    draw_map.strokeStyle="red";
-    draw_map.stroke();
+    let draw_map = canvas.canvas_event.getContext("2d");
+    let interval: number;
+    let length = line.length > 3 ? line.length :
+      (line.length * (line.HorV == horizontal_vertical.h ? canvas.canvas_event.width : canvas.canvas_event.height));
+
+    if (line.HorV == horizontal_vertical.h) {
+
+      interval = ((canvas.canvas_height - (init_point.y * 2)) / line.line_num);
+      for (let num = 0, coordinate_a = (init_point.y + interval * num), coordinate_b = init_point.x + length;
+        num < line.line_num;
+        num++, coordinate_a = (init_point.y + interval * num)) {
+        draw_map.beginPath();
+        draw_map.moveTo(init_point.x, coordinate_a);
+        draw_map.lineTo(coordinate_b, coordinate_a);
+        draw_map.stroke();
+      }
+    } else {
+
+      interval = ((canvas.canvas_width - (init_point.x * 2)) / line.line_num);
+      for (let num = 0, coordinate_a = (init_point.x + interval * num), coordinate_b = init_point.y + length;
+        num < line.line_num;
+        num++, coordinate_a = (init_point.x + interval * num)) {
+        draw_map.beginPath();
+        draw_map.moveTo(coordinate_a, init_point.y);
+        draw_map.lineTo(coordinate_a, coordinate_b);
+        draw_map.stroke();
+      }
+    }
     return true;
   }
 }
