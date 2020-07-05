7d276ef77350f9eabfb76e8799d5b91d8a96a770
ZhuHui <1076734229@qq.com>
2020-07-06 01:41:30 +0800

    修改获取canvas元素的方法，基本完成画线自适应的函数，之后单独抽离自适应函数并开放画线信息以供shade层观测使用，添加页面描述PPT：description.pptx

--- src/app/components/timeline/timeline.component.ts
+++ src/app/components/timeline/timeline.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { fromEvent } from 'rxjs';
 
 import * as datatype from './timeline_datatype';
 
-import { canvas_info, horizontal_vertical as HV, line_info } from '../../service/draw.service';
+import { canvas_info, horizontal_vertical as HV, line_info, horizontal_vertical } from '../../service/draw.service';
 import { DrawService } from '../../service/draw.service';
 
 
@@ -14,40 +14,95 @@ import { DrawService } from '../../service/draw.service';
 })
 export class TimelineComponent implements OnInit {
 
-  value_line: line_info[];
+  value_line: line_info[] = [{ start_point: { x: 0, y: 0 }, line_num: 3, length: 0, HorV: horizontal_vertical.v }];
   canvas: canvas_info = { canvas_height: 0, canvas_width: 0 };
   window_is_resize: boolean;
-  constructor(private draw: DrawService) { }
+  old_adaption_info = { height: -500, width: -500, w_ge_h: false };
+
+  constructor(private draw: DrawService, private render: Renderer2) { }
+
+  /**
+   * @todo self_adaption~~>timeline_self_adaption(adaption_info:{height:number,width:number,w_ge_h:boolean}),并抽成一个单独服务timeline_adaption.service)
+   */
+  private self_adaption(): void {
+    if (this.canvas.canvas_event) {
+      let adaption_info = {
+        height: this.canvas.canvas_event.parentElement.offsetHeight, width: this.canvas.canvas_event.parentElement.offsetWidth,
+        w_ge_h: this.canvas.canvas_event.parentElement.offsetWidth >= this.canvas.canvas_event.parentElement.offsetHeight
+      };
+      console.log(adaption_info.height, adaption_info.w_ge_h);
+      if ((Math.abs(adaption_info.height - this.old_adaption_info.height) >= 500) || (Math.abs(adaption_info.width - this.old_adaption_info.width) >= 500) || (adaption_info.w_ge_h != this.old_adaption_info.w_ge_h)) {
+        //所有线的长度都为0.95
+        this.value_line[0].length = 0.95;
+        /**
+        * 根据窗口大小自适应画线规则
+        */
+        if ((adaption_info.width <= 1366 && adaption_info.height <= 768) || (adaption_info.height <= 1366 && adaption_info.width <= 768)) {
+          this.value_line[0].line_num = 6;
+          adaption_info.width = 1366;
+          adaption_info.height = 768;
+        } else if ((adaption_info.width <= 1960 && adaption_info.height <= 1080) || (adaption_info.height <= 1960 && adaption_info.width <= 1080)) {
+          this.value_line[0].line_num = 12;
+          adaption_info.width = 1960;
+          adaption_info.height = 1080;
+        } else if ((adaption_info.width <= 2560 && adaption_info.height <= 1440) || (adaption_info.height <= 2560 && adaption_info.width <= 1440)) {
+          this.value_line[0].line_num = 12;
+          adaption_info.width = 2560;
+          adaption_info.height = 1440;
+        } else if ((adaption_info.width <= 3840 && adaption_info.height <= 1080) || (adaption_info.height <= 3840 && adaption_info.width <= 1080)) {
+          this.value_line[0].line_num = 24;
+          adaption_info.width = 3840;
+          adaption_info.height = 1080;
+        } else if ((adaption_info.width <= 4096 && adaption_info.height <= 3112) || (adaption_info.height <= 4096 && adaption_info.width <= 3112)) {
+          this.value_line[0].line_num = 18;
+          adaption_info.width = 4096;
+          adaption_info.height = 3112;
+        } else if ((adaption_info.width <= 7680 && adaption_info.height <= 4320) || (adaption_info.height <= 7680 && adaption_info.width <= 4320)) {
+          this.value_line[0].line_num = 24;
+          adaption_info.width = 7680;
+          adaption_info.height = 4320;
+        }
+        if (((2560 <= adaption_info.width && adaption_info.width <= 3840) && adaption_info.height <= 1080)) { }
+
+
+        if (adaption_info.w_ge_h) {
+          this.value_line[0].HorV = horizontal_vertical.v;
+          this.value_line[0].start_point.x = adaption_info.width * 0.02;
+          this.value_line[0].start_point.y = 40;
+
+        } else {
+          this.value_line[0].HorV = horizontal_vertical.h;
+          this.value_line[0].start_point.x = 30;
+          this.value_line[0].start_point.y = adaption_info.height * 0.01;
+        }
+
+        this.update(adaption_info.height, adaption_info.width);
+        this.old_adaption_info = adaption_info;
+      }
+    }
+  }
 
   /**
    * @description 改变窗口大小时，更新值的方法
    */
-  private update(): void {
-    if (this.canvas.canvas_event) {
-      this.canvas.canvas_height = this.canvas.canvas_event.parentElement.offsetHeight;
-      this.canvas.canvas_width = this.canvas.canvas_event.parentElement.offsetWidth;
-    }
-
-    /**
-     * @todo 根据窗口大小自适应画线规则
-     */
-    this.value_line = [{ line_num: 12, length: 0.8, HorV: HV.v }, { line_num: 12, length: 0.9, HorV: HV.h }];
+  private update(height: number, width: number): void {
+    this.canvas.canvas_height = height;
+    this.canvas.canvas_width = width;
+    this.draw.drawEquidistantLine(this.canvas, this.value_line);
   }
 
   ngOnInit(): void {
-    this.draw.setInitPoint(40, 100);
+    this.canvas.canvas_event = this.render.selectRootElement("#timeline_canvas");
+    this.self_adaption();
+
+    fromEvent(window, 'resize').subscribe((event) => {
+      //这里表示当窗口大小发生变化时所做的事，也就是说可以对多个图表进行大小调整
+      this.self_adaption();
+      //this.draw.drawEquidistantLine(this.canvas, this.value_line);
+    })
   }
 
   ngAfterViewInit(): void {
-    //加载完页面后才能获取到canvas元素
-    this.canvas.canvas_event = <HTMLCanvasElement>document.getElementById("myCanvas");
-    this.update();
-    this.draw.drawEquidistantLine(this.canvas, this.value_line);
-    fromEvent(window, 'resize').subscribe((event) => {
-      //这里表示当窗口大小发生变化时所做的事，也就是说可以对多个图表进行大小调整
-      this.update();
-      this.draw.drawEquidistantLine(this.canvas, this.value_line);
-    })
   }
 
   ngAfterViewChecked() {
--- src/app/service/draw.service.ts
+++ src/app/service/draw.service.ts
@@ -21,12 +21,14 @@ export enum horizontal_vertical { h, v }
 /**
  * @interface line_info
  * @description 设置画线的参数
+ * @param start_point 设置线条的起始点，与设置方向相同的竖直同时设置起始和终止间隔
  * @param line_num 设置画线的数量
  * @param length 设置线条的长度，值小于1时，为canvas_info设置的相应长度的倍数，大于1则为线条实际长度
  *        @example 设置为0.8 且HorV设置为V是，长度实际为canvas_info.canvas_height * length
  * @param HorV 水平或垂直
  */
 export interface line_info {
+  start_point?: { x: number, y: number };
   line_num: number;
   length: number;
   HorV: horizontal_vertical;
@@ -35,7 +37,7 @@ export interface line_info {
 /**
  * @description 画线的初始坐标
  */
-const init_point = { x: 40, y: 40 };
+let init_point = { x: 40, y: 40 };
 
 @Injectable({
   providedIn: 'root'
@@ -44,14 +46,6 @@ const init_point = { x: 40, y: 40 };
 export class DrawService {
 
   constructor() { }
-
-  /**
-   * @description 设置画线的初始坐标
-   */
-  setInitPoint(x: number, y: number) {
-    init_point.x = x;
-    init_point.y = y;
-  }
   /**
    * @param {canvas_info} canvas 传入canvas信息
    * @param {line_info} line 设置画线信息
@@ -66,6 +60,8 @@ export class DrawService {
     let draw_map = canvas.canvas_event.getContext("2d");
     let interval: number;
     for (let line_info_num = 0; line_info_num < lines.length; line_info_num++) {
+      init_point.x = lines[line_info_num].start_point.x;
+      init_point.y = lines[line_info_num].start_point.y;
       let length = lines[line_info_num].length < 0 ? 0 :
         lines[line_info_num].length > 1 ? lines[line_info_num].length :
           (lines[line_info_num].length * (lines[line_info_num].HorV == horizontal_vertical.h ? canvas.canvas_event.width : canvas.canvas_event.height));
