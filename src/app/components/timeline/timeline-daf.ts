d6c28b80cca7c9062da219524ae34243c5c2821d
ZhuHui <1076734229@qq.com>
2020-07-12 00:23:06 +0800

    完成绘制时间和线条的关系

--- src/app/components/timeline/timeline-daf.ts
+++ src/app/components/timeline/timeline-daf.ts
@@ -11,38 +11,38 @@ class TimelineDataAndFunction {
     /**
     * @description 常见比例及对应分割方法抽成数组,便于维护
     */
-    private resolution_ratio_and_divide: { w: number, h: number, n: number }[] = [
-        { w: 800, h: 600, n: 4 },
-        { w: 1024, h: 768, n: 4 },
-        { w: 1280, h: 720, n: 4 },
-        { w: 1280, h: 800, n: 6 },
-        { w: 1280, h: 960, n: 4 },
-        { w: 1366, h: 768, n: 6 },
-        { w: 1440, h: 900, n: 6 },
-        { w: 1600, h: 900, n: 6 },
-        { w: 1600, h: 1200, n: 4 },
-        { w: 1680, h: 1050, n: 6 },
-        { w: 1792, h: 868, n: 6 },
-        { w: 1920, h: 1080, n: 12 },
-        { w: 1920, h: 1200, n: 12 },
-        { w: 1920, h: 1400, n: 8 },
-        { w: 2048, h: 1152, n: 12 },
-        { w: 2048, h: 1536, n: 8 },
-        { w: 2560, h: 1080, n: 16 },
-        { w: 2560, h: 1440, n: 12 },
-        { w: 2560, h: 1600, n: 12 },
-        { w: 2800, h: 2100, n: 8 },//一下显示分辨率纯靠猜测,无实机测试
-        { w: 3440, h: 1440, n: 16 },
-        { w: 3840, h: 1080, n: 24 },
-        { w: 3840, h: 2160, n: 18 },
-        { w: 3840, h: 2400, n: 18 },
-        { w: 4096, h: 3072, n: 16 },
-        { w: 5120, h: 3200, n: 24 },
-        { w: 5120, h: 4096, n: 24 },
-        { w: 6400, h: 4096, n: 24 },
-        { w: 6400, h: 4800, n: 20 },
-        { w: 7680, h: 4320, n: 24 },
-        { w: 7680, h: 4800, n: 24 }
+    private resolution_ratio_and_divide: { w: number, h: number, n: number }[] = [/* 注释用来对齐数据*/
+        { w: 800  /**/, h: 600  /**/, n: 5  /**/ },
+        { w: 1024 /**/, h: 768  /**/, n: 5  /**/ },
+        { w: 1280 /**/, h: 720  /**/, n: 5  /**/ },
+        { w: 1280 /**/, h: 800  /**/, n: 7  /**/ },
+        { w: 1280 /**/, h: 960  /**/, n: 5  /**/ },
+        { w: 1366 /**/, h: 768  /**/, n: 7  /**/ },
+        { w: 1440 /**/, h: 900  /**/, n: 7  /**/ },
+        { w: 1600 /**/, h: 900  /**/, n: 7  /**/ },
+        { w: 1600 /**/, h: 1200 /**/, n: 5  /**/ },
+        { w: 1680 /**/, h: 1050 /**/, n: 7  /**/ },
+        { w: 1792 /**/, h: 868  /**/, n: 7  /**/ },
+        { w: 1920 /**/, h: 1080 /**/, n: 13 /**/ },
+        { w: 1920 /**/, h: 1200 /**/, n: 13 /**/ },
+        { w: 1920 /**/, h: 1400 /**/, n: 9  /**/ },
+        { w: 2048 /**/, h: 1152 /**/, n: 13 /**/ },
+        { w: 2048 /**/, h: 1536 /**/, n: 9  /**/ },
+        { w: 2560 /**/, h: 1080 /**/, n: 17 /**/ },
+        { w: 2560 /**/, h: 1440 /**/, n: 13 /**/ },
+        { w: 2560 /**/, h: 1600 /**/, n: 13 /**/ },
+        { w: 2800 /**/, h: 2100 /**/, n: 9  /**/ },//一下显示分辨率纯靠猜测,无实机测试
+        { w: 3440 /**/, h: 1440 /**/, n: 17 /**/ },
+        { w: 3840 /**/, h: 1080 /**/, n: 25 /**/ },
+        { w: 3840 /**/, h: 2160 /**/, n: 19 /**/ },
+        { w: 3840 /**/, h: 2400 /**/, n: 19 /**/ },
+        { w: 4096 /**/, h: 3072 /**/, n: 17 /**/ },
+        { w: 5120 /**/, h: 3200 /**/, n: 25 /**/ },
+        { w: 5120 /**/, h: 4096 /**/, n: 25 /**/ },
+        { w: 6400 /**/, h: 4096 /**/, n: 25 /**/ },
+        { w: 6400 /**/, h: 4800 /**/, n: 21 /**/ },
+        { w: 7680 /**/, h: 4320 /**/, n: 25 /**/ },
+        { w: 7680 /**/, h: 4800 /**/, n: 25 /**/ }
     ];
     /**
     * @description 比例及对应分割表的数据个数
@@ -51,7 +51,7 @@ class TimelineDataAndFunction {
     /**
     * @description timeline界面背景层的画线的信息
     */
-    private line_info: line_info = { start_point: { x: 0, y: 0 }, line_num: 3, length: 0.95, HorV: horizontal_vertical.v, follow_time: { is_draw_time: false, time_array: [], follow_style_h: follow_style.top, follow_style_v: follow_style.top } };
+    private line_info: line_info = { start_point: { x: 0, y: 0 }, line_num: 3, length: 0, HorV: horizontal_vertical.v, follow_time: { is_draw_time: false, time_array: [], follow_style_h: follow_style.right, follow_style_v: follow_style.top } };
     /**
     * @description 保存上一次更新canvas画布宽高时,所在的像素区域
     */
@@ -59,7 +59,7 @@ class TimelineDataAndFunction {
     /**
     * @description line_info.time_array的起始时间,单位为分,范围0~~1440;
     */
-    private timeline_start_time = 180;
+    private timeline_start_time = 90;
 
     constructor(private draw: DrawService) { }
     //所有线的长度都为0.95
@@ -87,7 +87,7 @@ class TimelineDataAndFunction {
                     this.line_info.line_num = this.resolution_ratio_and_divide[rrad_i].n;
                     this.line_info.follow_time.is_draw_time = is_draw_time;
                     if (is_draw_time) {
-                        this.line_info.follow_time.time_array = this.draw.getTimeArrayOneDay(this.line_info.line_num, this.timeline_start_time);
+                        this.line_info.follow_time.time_array = this.draw.getTimeArrayOneDay(this.line_info.line_num - 1, this.timeline_start_time);
                     }
                     if (adaption_info.w_ge_h) {
                         this.line_info.HorV = horizontal_vertical.v;
@@ -97,7 +97,7 @@ class TimelineDataAndFunction {
                         area_info.area_height = adaption_info.height;
                     } else {
                         this.line_info.HorV = horizontal_vertical.h;
-                        this.line_info.start_point.x = 30;
+                        this.line_info.start_point.x = 60;
                         this.line_info.start_point.y = (0.5 + adaption_info.height * 0.02) | 0;
                         area_info.area_width = adaption_info.height;
                         area_info.area_height = adaption_info.width;
--- src/app/service/draw.service.ts
+++ src/app/service/draw.service.ts
@@ -17,9 +17,9 @@ enum follow_style { top, bottom, left, right }
 /**
  * @description 设置画线的参数
  * @param line_num 设置画线的数量
- * @param length 设置线条的长度，值小于1时，为canvas_info设置的相应长度的倍数，大于1则为线条实际长度
+ * @param length 设置线条的长度，则值小于1时，为canvas_info设置的相应长度的倍数，大于1则为线条实际长度，值为0时,终点完全与起点相对应
  *        @example 设置为0.8 且HorV设置为V是，长度实际为canvas_info.canvas_height * length
- * @param start_point 设置线条的起始点，与设置方向相同的竖直同时设置起始和终止间隔
+ * @param start_point 设置线条的起点，长度为0时,终点完全与起点相对应
  * @param HorV 水平或垂直
  */
 interface line_info {
@@ -48,7 +48,7 @@ class DrawService {
    * @description 画线的初始坐标
    */
   private init_point = { x: 40, y: 40 };
-
+  private text_size = 15;
   constructor() { }
   /**
    * @description 得到时间表数组
@@ -57,27 +57,19 @@ class DrawService {
    */
   getTimeArrayOneDay(divide_part: number, start_time: number = 180): string[] {
     let s_time = start_time <= 0 ? 0 : start_time >= 1440 ? 0 : start_time, time_array: string[] = [], time_interval = (1440 / divide_part), time_h = ((s_time / 60) | 0), time_m = (start_time - time_h * 60);
-    time_array.push(((time_h > 10 ? "" + time_h : "0" + time_h) + ":" + (time_m > 10 ? "" + time_m : "0" + time_m)));
-    for (let divide_num = 1; divide_num < divide_part; divide_num++) {
+    time_array.push(((time_h >= 10 ? "" + time_h : "0" + time_h) + ":" + (time_m >= 10 ? "" + time_m : "0" + time_m)));
+    for (let divide_num = 0; divide_num < divide_part; divide_num++) {
       time_m = time_m + time_interval;
       while (time_m >= 60) {
         time_h += 1;
         time_m -= 60;
       }
-      time_array.push(((time_h > 10 ? "" + time_h : "0" + time_h) + ":" + (time_m > 10 ? "" + time_m : "0" + time_m)));
+      time_h = time_h >= 24 ? time_h - 24 : time_h;
+      time_array.push(((time_h >= 10 ? "" + time_h : "0" + time_h) + ":" + (time_m >= 10 ? "" + time_m : "0" + time_m)));
     }
     return time_array;
   }
-  /**
-   * @description 给定坐标点和对其方式,绘制文字
-   * @param {CanvasRenderingContext2D} draw_map 传入可绘制区域信息,即 canvas.getContext('2D')
-   * @param draw_Text 要绘制的文字
-   * @param draw_align 文字对齐方式
-   * @param draw_point 文字对齐的坐标
-   */
-  drawText(draw_map: CanvasRenderingContext2D, draw_Text: string, draw_align: follow_style, draw_point: { x: number, y: number }) {
 
-  }
   /**
    * @description 画出等间距的线
    * @param {CanvasRenderingContext2D} draw_map 传入可绘制区域信息,即 canvas.getContext('2D')
@@ -85,18 +77,25 @@ class DrawService {
    * @returns boolean 返回是否执行成功
    */
   drawEquidistantLine(draw_map: CanvasRenderingContext2D, canvas_width: number, canvas_height: number, line_infos: line_info[]) {
-    draw_map.setLineDash([9, 3]);
+    draw_map.font = this.text_size + "px cascadia code PL";
     draw_map.lineWidth = 1;
-    draw_map.font = "15px cascadia code PL";
     let line_interval: number;
     for (let line_info_num = 0; line_info_num < line_infos.length; line_info_num++) {
       this.init_point.x = line_infos[line_info_num].start_point.x;
       this.init_point.y = line_infos[line_info_num].start_point.y;
-      let length = line_infos[line_info_num].length < 0 ? 0 :
-        line_infos[line_info_num].length > 1 ? line_infos[line_info_num].length :
-          (0.5 + line_infos[line_info_num].length * (line_infos[line_info_num].HorV == horizontal_vertical.h ? canvas_width : canvas_height)) | 0;
+      let length: number;
+
+      if (line_infos[line_info_num].length <= 0) {
+        length = (line_infos[line_info_num].HorV == horizontal_vertical.h ? (canvas_width - this.init_point.x * 2) : (canvas_height - this.init_point.y * 2));
+      } else if (line_infos[line_info_num].length <= 1) {
+        length = (0.5 + line_infos[line_info_num].length * (line_infos[line_info_num].HorV == horizontal_vertical.h ? canvas_width : canvas_height)) | 0;
+      }
+      else {
+        length = line_infos[line_info_num].length;
+      }
+
       if (line_infos[line_info_num].HorV == horizontal_vertical.h) {
-        line_interval = ((canvas_height - (this.init_point.y * 2)) / (line_infos[line_info_num].line_num - 1));
+        line_interval = ((0.5 + (canvas_height - (this.init_point.y * 2)) / (line_infos[line_info_num].line_num - 1)) | 0);
         for (let num = 0, coordinate_a = (this.init_point.y + line_interval * num), coordinate_b = this.init_point.x + length, line_num = line_infos[line_info_num].line_num;
           num < line_num;
           num++, coordinate_a = (this.init_point.y + line_interval * num)) {
@@ -104,16 +103,59 @@ class DrawService {
           draw_map.moveTo(this.init_point.x, coordinate_a);
           draw_map.lineTo(coordinate_b, coordinate_a);
           draw_map.stroke();
+          if (line_infos[line_info_num].follow_time.is_draw_time) {
+            let fs = line_infos[line_info_num].follow_time.follow_style_h;
+            let point = { x: this.init_point.x - 10, y: coordinate_a - 2 };
+            switch (fs) {
+              case follow_style.top:
+                draw_map.textAlign = "start";
+                break;
+              case follow_style.bottom:
+                draw_map.textAlign = "start";
+                point.y += this.text_size + 4;
+                break;
+              case follow_style.left:
+                draw_map.textAlign = "end";
+                point.y += (this.text_size / 2) | 0;
+                break;
+              case follow_style.right:
+                draw_map.textAlign = "start";
+                point.x += length + 20;
+                point.y += (this.text_size / 2) | 0;
+                break;
+            }
+            draw_map.fillText(line_infos[line_info_num].follow_time.time_array[num], point.x, point.y);
+          }
         }
       } else {
-        line_interval = ((canvas_width - (this.init_point.x * 2)) / (line_infos[line_info_num].line_num - 1));
-        for (let num = 0, coordinate_a = (this.init_point.x + line_interval * num), coordinate_b = this.init_point.y + length, line_num = line_infos[line_info_num].line_num;
+        line_interval = (0.5 + ((canvas_width - (this.init_point.x * 2)) / (line_infos[line_info_num].line_num - 1)) | 0);
+        for (let num = 0, coordinate_a = 0.5 + (this.init_point.x + line_interval * num), coordinate_b = this.init_point.y + length, line_num = line_infos[line_info_num].line_num;
           num < line_num;
           num++, coordinate_a = (this.init_point.x + line_interval * num)) {
           draw_map.beginPath();
           draw_map.moveTo(coordinate_a, this.init_point.y);
           draw_map.lineTo(coordinate_a, coordinate_b);
           draw_map.stroke();
+          if (line_infos[line_info_num].follow_time.is_draw_time) {
+            let fs = line_infos[line_info_num].follow_time.follow_style_v;
+            let point = { x: coordinate_a, y: this.init_point.y - 2 };
+            switch (fs) {
+              case follow_style.top:
+                draw_map.textAlign = "center";
+                break;
+              case follow_style.bottom:
+                draw_map.textAlign = "center";
+                point.y += length + this.text_size + 4;
+                break;
+              case follow_style.left:
+                draw_map.textAlign = "end";
+                break;
+              case follow_style.right:
+                draw_map.textAlign = "start";
+                break;
+            }
+            draw_map.fillText(line_infos[line_info_num].follow_time.time_array[num], point.x, point.y);
+          }
         }
       }
     }
