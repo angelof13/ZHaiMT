c312d8cc9c665d5220ca7c6d5d2428ccecd57771
yuge <sssyuge@outlook.com>
2020-07-17 21:31:17 +0800

    删除遮罩元素及相关方法，修改canvas增加滤镜的方法，优化资源占用

--- src/app/components/shade/shade.component.ts
+++ src/app/components/shade/shade.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Renderer2 } from '@angular/core';
 import { ShadeService } from '../../service/shade.service';
-import { fromEvent,interval } from 'rxjs';
+import { fromEvent,interval, Subscription, timer } from 'rxjs';
 
 
 
@@ -21,15 +21,7 @@ export class ShadeComponent implements OnInit {
   
   ngOnInit(): void {
 
-    const secondsCounter = interval(1000);
-    // Subscribe to begin publishing values
-    let shade_canvas =this.render.selectRootElement("#shade_canvas");
-    secondsCounter.subscribe(n =>{
-      this.shade_service.current_time_part_shade_canvas(shade_canvas,this.eject_direction);
-      console.log(this.eject_direction+n);
-      
-      
-    })
+   
     
 
   }
@@ -43,16 +35,17 @@ export class ShadeComponent implements OnInit {
       this.eject_direction = eject_direction;
       this.shade_init(eject_direction);
     })
+
+    
   }
 
   shade_init(eject_direction: string){
     var date = new Date();
     // 计算当前毫秒票数
     var milliseconds = (date.getTime()+28800000)%86400000;
-    // 遮罩的弹出方法
-    this.shade_service.makeAnimation(this.render.selectRootElement("#shade"),0.5,"black",milliseconds,eject_direction);
     let canvas = this.render.selectRootElement("#myCanvas");
     let shade_canvas =this.render.selectRootElement("#shade_canvas");
+
     // 获取被复制的canvas的长和宽
     let width = canvas.parentElement.offsetWidth;
     let height = canvas.parentElement.offsetHeight;
@@ -69,6 +62,9 @@ export class ShadeComponent implements OnInit {
     this.shade_service.current_time_shade_canvas(shade_canvas,eject_direction)
   }
 
+  /**
+   * @description 判断渲染方向
+   */
   get_eject_direction(): string {
     let height: number = document.body.offsetHeight;
     let width: number = document.body.offsetWidth;
--- src/app/service/shade.service.ts
+++ src/app/service/shade.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Renderer2 } from '@angular/core';
 import { animate,keyframes,style, trigger,state, transition ,AnimationBuilder} from '@angular/animations';
+import { __core_private_testing_placeholder__ } from '@angular/core/testing';
+import { fromEvent,interval, Subscription, timer } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShadeService {
+
+  // 快速渲染计时器观察者
+  private fast_shade : Subscription;
+
+  // 慢速渲染计时器观察者
+  private slow_shade : Subscription;
+
+  private fast_timer = timer(0,10);
+
+  private slow_timer =  timer(0,1000);
+
 
   constructor(private shade_builder :AnimationBuilder) { }
 
@@ -27,7 +40,7 @@ export class ShadeService {
 
     // 循环遍历像素点并将相应的rgba的值设为这四种rgba平均值
     for(var i=0; i<imageData.data.length-4; i=i+4){
-      let average=(imageData.data[i]+imageData.data[i+1]+imageData.data[i+2])/3;
+      let average=(imageData.data[i] + (imageData.data[i+1] << 1) + imageData.data[i+2] ) >> 2;
       imageData.data[i]=average;
       imageData.data[i+1]=average;
       imageData.data[i+2]=average;
@@ -39,67 +52,6 @@ export class ShadeService {
 
   }
 
-    /**
-   * @param opacity 遮罩的透明度
-   * @param color   遮罩的颜色
-   * @param element 动画绑定的元素
-   * @param current_millisecend  当前的毫秒数
-   * @param eject_direction 遮罩弹出方向
-   * @description 设置遮罩动画的效果
-   */
-  makeAnimation(element: any,opacity:number,color: string,current_millisecend: number,eject_direction: string) {
-
-    // 弹出的百分比
-    let percentage = (current_millisecend/86400000+0.002)*100
-
-
-    // 触发关键帧的位置
-    let offset = 2000/(86400000-current_millisecend+2000)
-
-  
-    const myAnimation =eject_direction=="left"? this.shade_builder.build([
-
-      animate(86400000-current_millisecend+2000+"ms" , keyframes([
-        style({ background: color,height: "100%",position:"absolute",bottom:0,width:"0.2%",opacity: opacity, offset: 0 }),
-        style({ background: color,height: "100%",position:"absolute",bottom:0,width:percentage+"%",opacity: opacity, offset: offset }),
-        style({ background: color,height: "100%",position:"absolute",bottom:0,width:"99.6%",opacity: opacity, offset: 1 }),
-      ]))
-    ])
-    : eject_direction=="top"? this.shade_builder.build([
-
-      animate(86400000-current_millisecend+2000+"ms" , keyframes([
-        style({ background: color,width: "100%",position:"absolute",top:0,height:"0.2%",opacity: opacity, offset: 0 }),
-        style({ background: color,width: "100%",position:"absolute",top:0,height:percentage+"%",opacity: opacity, offset: offset }),
-        style({ background: color,width: "100%",position:"absolute",top:0,height:"99.6%",opacity: opacity, offset: 1 }),
-      ]))
-    ])
-    : eject_direction=="right"? this.shade_builder.build([
-
-      animate(86400000-current_millisecend+2000+"ms" , keyframes([
-        style({ background: color,height:"100%",position:"absolute",bottom:0,margin:"0px 0px 0px 99.8%",width:"0.2%",opacity: opacity, offset: 0 }),
-        style({ background: color,height:"100%",position:"absolute",bottom:0,margin:"0px 0px 0px "+(100-percentage)+"%",width:percentage+"%",opacity: opacity, offset: offset }),
-        style({ background: color,height:"100%",position:"absolute",bottom:0,margin:"0px 0px 0px 0.4%",width:"99.6%",opacity: opacity, offset: 1 }),
-      ]))
-    ])
-    :eject_direction == "bottom"? this.shade_builder.build([
-
-      animate(86400000-current_millisecend+2000+"ms" , keyframes([
-        style({ background: color,position:"absolute",bottom:0,width:"100%",opacity: opacity, offset: 0 }),
-        style({ background: color,position:"absolute",bottom:0,width:"100%",height:percentage+"%",opacity: opacity, offset: offset }),
-        style({ background: color,position:"absolute",bottom:0,width:"100%",height:"99.6%",opacity: opacity, offset: 1 }),
-      ]))
-    ])
-    : this.shade_builder.build([
-      style({ width: 0,background: "black" }),
-      animate(86400000-current_millisecend+2000+"ms")
-    ])
-    ;
-
-    const player = myAnimation.create(element);
-    
-    player.play();
-  }
-
 
   /**
    * 
@@ -109,26 +61,84 @@ export class ShadeService {
    */
   current_time_shade_canvas(canvas : HTMLCanvasElement, eject_direction: string){
 
+    let that = this;
+    
     let width = canvas.parentElement.offsetWidth;
     let height = canvas.parentElement.offsetHeight;
-    var date = new Date();
-    var milliseconds = (date.getTime()+28800000)%86400000;
+    
+    let date = new Date();
+    let milliseconds = (date.getTime()+28800000)%86400000;
     let percentage = milliseconds/86400000+0.002
+    
+    // 宽度增加1px的时间
+    let width_time =  86400000/(width*0.996);
+    // 高度增加1px的时间
+    let height_time = 86400000/(width*0.996);
+    if(this.fast_shade){
+      this.fast_shade.unsubscribe()
+    }
     switch (eject_direction) {
       case "top":
-        this.canvasBlackAndWhiteFilters(canvas,0,0,width,height*percentage);
+        this.fast_shade = this.fast_timer.subscribe(n=>{
+          that.canvasBlackAndWhiteFilters(canvas,0,n,width,1);
+          if(n>height*percentage){
+            that.fast_shade.unsubscribe();
+            that.slow_timer = timer(0,height_time);
+            that.slow_shade = that.slow_timer.subscribe(n=>{
+              that.canvasBlackAndWhiteFilters(canvas,0,n+height*percentage,width,1);
+              if(n>height-height*percentage){
+                that.slow_shade.unsubscribe()
+              }
+            })
+          }
+        })
         break;
       case "bottom":
 
-        this.canvasBlackAndWhiteFilters(canvas,0,height*(1-percentage),width,height*percentage);
+        this.fast_shade = this.fast_timer.subscribe(n=>{
+          that.canvasBlackAndWhiteFilters(canvas,0,height-n,width,1);
+          if(n>height*percentage){
+            that.fast_shade.unsubscribe();
+            that.slow_timer = timer(0,height_time);
+            that.slow_shade = that.slow_timer.subscribe(n=>{
+              that.canvasBlackAndWhiteFilters(canvas,0,height-height*percentage-n,width,1);
+              if(n>height-height*percentage){
+                that.slow_shade.unsubscribe()
+              }
+            })
+          }
+        })
         break;
       case "right":
 
-        this.canvasBlackAndWhiteFilters(canvas,width*(1-percentage),0,width*percentage,height);
+        this.fast_shade = this.fast_timer.subscribe(n=>{
+          that.canvasBlackAndWhiteFilters(canvas,width-n,0,1,height);
+          if(n>width*percentage){
+            that.fast_shade.unsubscribe();
+            that.slow_timer = timer(0,width_time);
+            that.slow_shade = that.slow_timer.subscribe(n=>{
+              that.canvasBlackAndWhiteFilters(canvas,width-width*percentage-n,0,1,height);
+              if(n>width-width*percentage){
+                that.slow_shade.unsubscribe()
+              }
+            })
+          }
+        })
         break;
       case "left":
-
-        this.canvasBlackAndWhiteFilters(canvas,0,0,width*percentage,height);
+        this.fast_shade = this.fast_timer.subscribe(n=>{
+          that.canvasBlackAndWhiteFilters(canvas,n,0,1,height);
+          if(n>width*percentage){
+            that.fast_shade.unsubscribe();
+            that.slow_timer = timer(0,width_time)
+            that.slow_shade = that.slow_timer.subscribe(n=>{
+              that.canvasBlackAndWhiteFilters(canvas,n+width*percentage,0,1,height);
+              if(n>width-width*percentage){
+                that.slow_shade.unsubscribe()
+              }
+            })
+          }
+        })
         break;
     
       default:
@@ -136,35 +146,4 @@ export class ShadeService {
     }
 
   }
-
-  current_time_part_shade_canvas(canvas : HTMLCanvasElement, eject_direction: string){
-
-    let width = canvas.parentElement.offsetWidth;
-    let height = canvas.parentElement.offsetHeight;
-    var date = new Date();
-    var milliseconds = (date.getTime()+28800000)%86400000;
-    let percentage = milliseconds/86400000+0.002
-    switch (eject_direction) {
-      case "top":
-        this.canvasBlackAndWhiteFilters(canvas,0,0,width,height*percentage);
-        break;
-      case "bottom":
-
-        this.canvasBlackAndWhiteFilters(canvas,0,height*(1-percentage),width,height*percentage);
-        break;
-      case "right":
-
-        this.canvasBlackAndWhiteFilters(canvas,width*(1-percentage),0,width*percentage,height);
-        break;
-      case "left":
-
-        this.canvasBlackAndWhiteFilters(canvas,0,width*percentage-1,2,height);
-        break;
-    
-      default:
-        break;
-    }
-
-  }
-
 }
