b7ec12b0d54a9a562d6fcb3925f68cac21dbfdeb
flourishdream <flourishdream@qq.com>
2021-04-12 13:50:55 +0800

    update angular/cli

--- src/app/components/icp/icp.component.spec.ts
+++ src/app/components/icp/icp.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ICPComponent } from './icp.component';
 
@@ -6,7 +6,7 @@ describe('ICPComponent', () => {
   let component: ICPComponent;
   let fixture: ComponentFixture<ICPComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ICPComponent ]
     })
